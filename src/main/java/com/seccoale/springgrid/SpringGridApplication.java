7b3183d01d7a5b9488739ab6eab25d29e09a329f
seccoale <seccoale@gmail.com>
2017-08-03 10:42:02 +0200

    Project Structure with Spring Initializr and some random packages

---
+++ src/main/java/com/seccoale/springgrid/SpringGridApplication.java
@@ -0,0 +1,12 @@
+package com.seccoale.springgrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGridApplication.class, args);
+	}
+}
